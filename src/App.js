7206cf10dc53d0ffda1459eede2714a3f06ce4d0
devmitul <mitulnaliyadhara2000@gmail.com>
2023-12-24 16:15:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+import { Footer, Blog, Header, Features, Possibility, WhatGPT3 } from './containers';
+import { Brand, CTA, Navbar } from './components';
+
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+
+
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog01 from "../../assets/01.png"
+import blog02 from "../../assets/02.png"
+import blog03 from "../../assets/03.png"
+import blog04 from "../../assets/04.png"
+import blog05 from "../../assets/05.png"
+
+export{
+        blog01,
+        blog02,
+        blog03,
+        blog04,
+        blog05
+    }
